74a63e2ea564ee311b8a16d7ab13fe9e4aaa1ff3
Jakub <jakubjagielski8@gmail.com>
2022-07-25 20:20:05 +0200

    WIP

--- backend/api/sudoku_solver_visual.py
+++ backend/api/sudoku_solver_visual.py
@@ -7,14 +7,15 @@ import cv2
 import fastapi
 import numpy as np
 import tensorflow.keras.models
+import os
 
 WIDTH_IMG = 450
 HEIGHT_IMG = 450
 
-MODEL = tensorflow.keras.models.load_model("model_trained.h5")
+MODEL = tensorflow.keras.models.load_model("backend/api/model_trained.h5")
 
 
-def find_corners_of_biggest_contour(contours) -> np.array | None:
+def find_corners_of_biggest_contour(contours) -> np.ndarray | None:
     biggest_contour_corners = None
     biggest_area_found = 0
 
@@ -34,7 +35,7 @@ def find_corners_of_biggest_contour(contours) -> np.array | None:
     return biggest_contour_corners
 
 
-def reorder_grid_corners(shuffled_corners: np.array) -> np.array:
+def reorder_grid_corners(shuffled_corners: np.array) -> np.ndarray:
     shuffled_corners = shuffled_corners.reshape((4, 2))
     reordered_corners = np.zeros((4, 1, 2), np.int32)
 
@@ -49,7 +50,7 @@ def reorder_grid_corners(shuffled_corners: np.array) -> np.array:
     return reordered_corners
 
 
-def find_grid_corners_from_contours(contours) -> np.array | None:
+def find_grid_corners_from_contours(contours) -> np.ndarray | None:
     shuffled_grid_corners = find_corners_of_biggest_contour(contours)
     if shuffled_grid_corners is None:
         return None
@@ -99,7 +100,9 @@ def draw_grid(img: np.ndarray, color=(0, 255 // 2, 0)) -> np.ndarray:
 
 def solve_sudoku_from_image(sudoku_image_raw: np.ndarray) -> np.ndarray:
     sudoku_image_original = sudoku_image_raw.copy()
-
+    sudoku_image_original_resized = cv2.resize(
+        sudoku_image_raw.copy(), (WIDTH_IMG, HEIGHT_IMG)
+    )
     print("Pre-processing board...")
     sudoku_image_sharpened = pre_process_board(sudoku_image_original)
     print("Extracting grid from image...")
@@ -107,6 +110,7 @@ def solve_sudoku_from_image(sudoku_image_raw: np.ndarray) -> np.ndarray:
         sudoku_image_sharpened, sudoku_image_raw
     )
     print("Grid extracted")
+    # cv2.imshow("grid", sudoku_grid_image_warped)
 
     print("Performing image recognition on grid...")
     sudoku_puzzle = extract_sudoku_string_from_grid_image(
@@ -124,7 +128,8 @@ def solve_sudoku_from_image(sudoku_image_raw: np.ndarray) -> np.ndarray:
 
     print("Preparing image to display")
     digits_to_display = [
-        int(sudoku_solution[i]) if sudoku_puzzle[i] == 0 else 0 for i in range(0, 81)
+        int(sudoku_solution[i]) if int(sudoku_puzzle[i]) == 0 else 0
+        for i in range(0, 81)
     ]
 
     solution_image = sudoku_grid_image_warped.copy()
@@ -135,8 +140,11 @@ def solve_sudoku_from_image(sudoku_image_raw: np.ndarray) -> np.ndarray:
 
     solution_image_to_overlay = unwarp_image(solution_image, grid_corners)
 
+    print(solution_image_to_overlay.shape)
+    print(sudoku_image_original.shape)
+    # return solution_image_to_overlay
     final_image = cv2.addWeighted(
-        solution_image_to_overlay, 1, sudoku_image_original, 0.3, 1
+        solution_image_to_overlay, 1, sudoku_image_original_resized, 0.3, 1
     )
     print("Returning image")
     return final_image
@@ -154,7 +162,7 @@ def unwarp_image(solution_image: np.ndarray, grid_corners) -> np.ndarray:
 
 
 def split_boxes(sudoku_grid_image: np.ndarray) -> list[np.ndarray]:
-    return [box for row in np.vsplit(sudoku_grid_image) for box in np.hsplit(row, 9)]
+    return [box for row in np.vsplit(sudoku_grid_image, 9) for box in np.hsplit(row, 9)]
 
 
 def prepare_image_for_classification(image: np.ndarray) -> np.ndarray:
@@ -170,10 +178,13 @@ def prepare_image_for_classification(image: np.ndarray) -> np.ndarray:
     return img
 
 
-def classify_digit(image: np.ndarray, model) -> int:
+def classify_digit(image: np.ndarray) -> int:
     predictions = MODEL.predict(image)
     class_index = np.argmax(predictions, axis=1)
-    return class_index[0]
+    probability = np.amax(predictions)
+    if probability > 0.7:
+        return class_index[0]
+    return 0
 
 
 def extract_sudoku_string_from_grid_image(sudoku_grid_image: np.ndarray) -> str:
@@ -186,7 +197,7 @@ def extract_sudoku_string_from_grid_image(sudoku_grid_image: np.ndarray) -> str:
 def extract_grid_and_corners_from_sudoku_image(
     sudoku_image_sharp: np.ndarray,
     sudoku_image_original: np.ndarray,
-) -> t.Tuple[np.ndarray | None, np.array | None]:
+) -> t.Tuple[np.ndarray | None, np.ndarray | None]:
     contours, _ = cv2.findContours(
         sudoku_image_sharp, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
@@ -214,7 +225,3 @@ def pre_process_board(sudoku_grid_image: np.ndarray) -> np.ndarray:
     img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
     img_threshold = cv2.adaptiveThreshold(img_blur, 255, 1, 1, 11, 2)
     return img_threshold
-
-
-def build_response_image(sudoku_solution: str, image: np.ndarray) -> np.ndarray:
-    pass
